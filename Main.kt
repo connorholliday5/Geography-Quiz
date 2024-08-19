b05002d1a782974e2fb4f140368a36ada37b3697
connorholliday5 <133919757+connorholliday5@users.noreply.github.com>
2024-08-19 10:41:40 -0400

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,13 @@
+package com.example.project1geography
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Nav()
+        }
+    }
+}
